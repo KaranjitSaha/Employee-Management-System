587e5df6eb8ddad74eb7787407d1531d13945418
KaranjitSaha <Karanjit.Saha@iiitb.ac.in>
2023-11-11 19:14:12 +0530

    created v1 of the application

---
+++ frontend1/src/services/EmployeeService.js
@@ -0,0 +1,25 @@
+import axios from 'axios'
+
+const EMPLOYEE_BASE_REST_API_URL = 'http://localhost:8082/api/v1/employees';
+
+class EmployeeService {
+    getAllEmployees() {
+        return axios.get(EMPLOYEE_BASE_REST_API_URL);
+    }
+    createEmployee(employee) {
+        return axios.post(EMPLOYEE_BASE_REST_API_URL, employee)
+    }
+
+    getEmployeeById(employeeId) {
+        return axios.get(EMPLOYEE_BASE_REST_API_URL + '/' + employeeId);
+    }
+
+    updateEmployee(employeeId, employee) {
+        return axios.put(EMPLOYEE_BASE_REST_API_URL + '/' + employeeId, employee);
+    }
+
+    deleteEmployee(employeeId) {
+        return axios.delete(EMPLOYEE_BASE_REST_API_URL + '/' + employeeId);
+    }
+}
+export default new EmployeeService();//we can directly import the object no need to create an instance of the object
