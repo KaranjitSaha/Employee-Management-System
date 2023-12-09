dbcc36c6bd9f5bd9fa081001661be5e57d8a3965
KaranjitSaha <Karanjit.Saha@iiitb.ac.in>
2023-12-09 18:45:10 +0530

    Added Testing of backend

---
+++ src/test/java/com/KJ/EmployeeControllerTests.java
@@ -0,0 +1,67 @@
+package com.KJ;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import java.util.Arrays;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import com.KJ.Employee_Management_System.EmployeeManagementSystemApplication;
+import com.KJ.Employee_Management_System.controller.EmployeeController;
+import com.KJ.Employee_Management_System.model.Employee;
+import com.KJ.Employee_Management_System.repository.EmployeeRepository;
+
+@SpringBootTest(classes = EmployeeManagementSystemApplication.class)
+// @WebMvcTest(SubmissionController.class)
+@AutoConfigureMockMvc
+public class EmployeeControllerTests {
+    @Mock
+    private EmployeeRepository employeeRepository;
+
+    @InjectMocks
+    private EmployeeController employeeController;
+
+    // Initialize mocks
+    public void EmployeeControllerTest() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void testGetAllEmployees() {
+        // Mock data
+        Employee employee1 = new Employee();
+        employee1.setId(1L);
+        employee1.setFirstName("John");
+        employee1.setLastName("Doe");
+        employee1.setEmailId("john.doe@example.com");
+
+        Employee employee2 = new Employee();
+        employee2.setId(2L);
+        employee2.setFirstName("Jane");
+        employee2.setLastName("Doe");
+        employee2.setEmailId("jane.doe@example.com");
+
+        List<Employee> mockEmployeeList = Arrays.asList(employee1, employee2);
+
+        // Mock the behavior of the employeeRepository
+        when(employeeRepository.findAll()).thenReturn(mockEmployeeList);
+
+        // Call the controller method
+        List<Employee> result = employeeController.getAllEmployees();
+
+        // Verify the result
+        assertEquals(mockEmployeeList.size(), result.size());
+        assertEquals(mockEmployeeList.get(0).getFirstName(), result.get(0).getFirstName());
+        assertEquals(mockEmployeeList.get(1).getEmailId(), result.get(1).getEmailId());
+        // Add more assertions as needed
+    
+	}
+}
